2bc22d3ff0bad764ced25b6c4598e3fbf2bed1ef
Joart Cano <joartcano@Joarts-MacBook-Pro.local>
2022-02-24 14:34:08 +0800

    Created iframe options middleware

--- src/interface/http/index.ts
+++ src/interface/http/index.ts
@@ -3,9 +3,9 @@ import path from 'path';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+
 import routes from './routes';
-import apiKeyMapper from '../../config/api-key-mapper';
-import config from '../../config';
+import { iframeOptions } from '../../middlewares/iframeOptions';
 
 const app = express();
 app.use(bodyParser.json());
@@ -13,22 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(cookieParser());
 // setup web front
-app.disable('view cache');
-app.use(function (req, res, next) {
-	const iframeReferrer = req.headers.referer;
-	const apiKey: string = (req.query?.api_key || '') as string;
-	const apiKeyRecord = (config as any)[apiKey] || [];
-
-	if (
-		!iframeReferrer ||
-		!apiKey ||
-		(!!apiKeyRecord && !apiKeyRecord.includes(iframeReferrer))
-	) {
-		res.append('X-Frame-Options', 'SAMEORIGIN');
-	}
-
-	next();
-});
+app.use(iframeOptions);
 app.use(
 	express.static(path.join(__dirname, '../../../public'), {
 		extensions: ['html'],
---
+++ src/middlewares/iframeOptions.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from 'express';
+import config from '../config';
+
+export const iframeOptions = (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	const iframeReferrer = req.headers.referer;
+	const apiKey: string = (req.query?.api_key || '') as string;
+
+	// Note: this should be fetch from invoice settings
+	// This was only used for demo purpose only
+	const validHosts = (config as any)[apiKey] || [];
+
+	// We make sure that there is an api_key query
+	// if the request is coming from different origin
+	// otherwise will denied the request.
+	if (
+		!iframeReferrer ||
+		!apiKey ||
+		(!!validHosts && !validHosts.includes(iframeReferrer))
+	) {
+		res.append('X-Frame-Options', 'SAMEORIGIN');
+	}
+
+	next();
+};
